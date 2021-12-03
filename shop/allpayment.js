84951d3d4d003d02d171f0ea6bcbf2081e3ab56b
Made <https://github.com/MadeSparkle/alphaproject.git>
2021-12-03 17:09:23 +0700

    alphaproject

---
+++ shop/allpayment.js
@@ -0,0 +1,22 @@
+const allpayment = (prefix) => {
+return `PAYMENT BY ALPHA BOT
+
+*💰 E-MONEY*
+
+1. GOPAY
+	ㅁ -
+2. OVO
+	ㅁ -
+3. DANA
+	ㅁ 0822 3256 8963
+
+*📲 PULSA*
+
+Tidak tersedia
+	
+Sebelum melakukan pembayaran ada baiknya anda menghubungi owner terlebih dahulu!
+`
+	}
+
+exports.allpayment = allpayment
+ 
---
+++ shop/donasi.js
@@ -0,0 +1,18 @@
+const donasibot = () => {
+return `DONASI BISA VIA BANK / E-MONEY
+
+*💰 E-MONEY*
+
+1. GOPAY
+	ㅁ -
+2. OVO
+	ㅁ -
+3. DANA
+	ㅁ 0822 3256 8963
+	
+ThankYou
+`
+	}
+
+exports.donasibot = donasibot
+ 
